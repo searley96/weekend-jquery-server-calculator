8c78f76d9455ff3ffd5aed43fb3edfc9b0186367
Siv Earley <searley96@gmail.com>
2023-03-04 14:51:43 -0600

    created listeners for the submit button and operators buttons and set up functions for post and get on server and client sides

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,109 @@
 $(document).ready(handleReady);
 
+// let calculations = [];
+let sign = ""
+
 function handleReady() {
     console.log("jquery is loaded!");
 
+    $("#equalsBtn").on("click", submitCalculation);
+
+    $("#additionBtn").on("click", addCalculation);
+
+    $("#subtractionBtn").on("click", subtractCalculation);
+
+    $("#multiplicationBtn").on("click", multiplyCalculation);
+
+    $("#divisionBtn").on("click", divideCalculation);
+
+}
+
+function addCalculation() {
+    sign = `+`;
+    console.log("sign:", sign);
+}
+
+function subtractCalculation() {
+    sign = `-`;
+    console.log("sign:", sign);
+}
+
+function multiplyCalculation() {
+    sign = `*`;
+    console.log("sign:", sign);
+}
+
+function divideCalculation() {
+    sign = `/`;
+    console.log("sign:", sign);
 }
 
 
+
+
+// function additionCalculation() {
+//    let addition =  (Number(submitCalculation.firstNumberInput) + Number(submitCalculation.secondNumberInput))
+//     console.log('addition calculation', addition)
+//    };
+
+function getCalculation() {
+    $.ajax({
+        method: 'GET',
+        url: '/calculations'
+    }).then(function(response){
+        // calculations = response;
+        // render();
+    }).catch (function(err){
+        alert('Unable to get calculations. Try again later.');
+        console.log(err)
+    })
+}
+
+function submitCalculation() {
+    //get values from input
+    let calculations = {
+        firstNumberInput: $('#firstNumberInputField').val(),
+        signOperator: sign,
+        secondNumberInput: $('#secondNumberInputField').val()
+    }
+   console.log('calculation', calculations);
+   //ajax to server
+   $.ajax({
+    method: 'POST',
+    url: '/addCalculations',
+    data: calculations
+   }).then((response)=> {
+    console.log('post finished')
+    $('#firstNumberInputField').val(""),
+    $('#secondNumberInputField').val("")
+    getCalculation()
+   
+   })
+//    render()
+   //deal with response
+};
+
+function render(){
+    $("#firstNumberInputField").empty();
+    $("#secondNumberInputField").empty();
+
+
+    for (let calculation of calculations) {
+        $("#calculationHistory").append(`
+        <ul>
+            <li>${calculation.firstNumberInput} ${calculation.signOperator} ${calculation.secondNumberInput}</li>
+        </ul>
+        `)
+
+    }
+
+//     $("calucaltionTotal").append(`
+    
+//     `)
+
+    // $("calculationHistory").append(`
+    // <ul>
+    //     <li>${submitCalculation.firstNumberInput} ${submitCalculation.secondNumberInput}</li>
+    // </ul>
+    // `)
+}
--- server/server.js
+++ server/server.js
@@ -13,3 +13,30 @@ app.use(express.static('server/public'));
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
   })
+
+  //empty array for data
+  let calculations = [];
+
+  // define routes (endpoints)
+  app.get('/calculations', (req, res) => {
+    console.log('in /calculations GET')
+    res.send(calculations)
+  });
+
+  //POST
+  app.post('/addCalculations', (req, res) => { 
+    console.log('in post', req.body);
+    let firstNumberInput = Number(req.body.firstNumberInput);
+    let sign = (req.body.sign);
+    let secondNumberInput = Number(req.body.secondNumberInput);
+    
+
+    calculations.push(req.body);
+    res.sendStatus(201);
+    
+    //LOGIC GOES HERE
+    if ()
+
+
+
+  });
