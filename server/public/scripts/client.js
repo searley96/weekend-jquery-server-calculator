04c322914d234b43270518cb9e657b2705777250
Siv Earley <searley96@gmail.com>
2023-03-05 16:47:03 -0600

    formatted code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -23,38 +23,36 @@ function handleReady() {
 
     $("#clearBtn").on("click", deleteInputFields);
 
-}
+};
 
 
 
 function addCalculation() {
     sign = `+`;
     console.log("sign:", sign);
-}
+};
 
 function subtractCalculation() {
     sign = `-`;
     console.log("sign:", sign);
-}
+};
 
 function multiplyCalculation() {
     sign = `*`;
     console.log("sign:", sign);
-}
+};
 
 function divideCalculation() {
     sign = `/`;
     console.log("sign:", sign);
-}
+};
 
 function deleteInputFields() {
     sign = `C`;
     console.log("sign:", sign);
     $('#firstNumberInputField').val(""),
-    $('#secondNumberInputField').val("")
-   };
-
-
+        $('#secondNumberInputField').val("")
+};
 
 function getCalculation() {
     $.ajax({
@@ -63,12 +61,12 @@ function getCalculation() {
     }).then((response) => {
         calculations = response;
         render(response);
-    }).catch (function(err){
+    }).catch(function (err) {
         alert('Unable to get calculations. Try again later.');
         console.log(err)
     })
-   
-}
+
+};
 
 function submitCalculation() {
     //get values from input
@@ -77,43 +75,41 @@ function submitCalculation() {
         signOperator: sign,
         secondNumberInput: $('#secondNumberInputField').val(),
     }
+    //ajax to server
+    $.ajax({
+        method: 'POST',
+        url: '/addCalculations',
+        data: calculation
+    }).then((response) => {
+        console.log('post finished')
+        getCalculation()
 
-   console.log('calculation', calculation);
-   //ajax to server
-   $.ajax({
-    method: 'POST',
-    url: '/addCalculations',
-    data: calculation
-   }).then((response)=> {
-    console.log('post finished')
-    getCalculation()
-   
-   })
+    });
 
 };
 
 
 
-function render(){
-   
+function render() {
+
     $("#calucaltionTotal").empty();
     $("#calculationHistory").empty();
-   
+
 
     $("#calucaltionTotal").append(`
-    <h2>${calculations[calculations.length-1].answer}</h2>
-    `)
+    <h2>${calculations[calculations.length - 1].answer}</h2>
+    `);
 
     for (let values of calculations) {
-        
+
 
         $("#calculationHistory").append(`
         <ul>
             <li>${values.firstNumberInput} ${values.signOperator} ${values.secondNumberInput} = ${values.answer}</li>
         </ul>
-        `)
+        `);
 
-    }
+    };
 
-    
-}
+
+};
--- server/server.js
+++ server/server.js
@@ -6,87 +6,74 @@ const PORT = 5000;
 console.log("server file running");
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
-    console.log ('Server is running on port', PORT)
-  })
+  console.log('Server is running on port', PORT)
+})
 
-  
-  
-  // empty array for data
-//   let calculations = [
-//     {
-//     firstNumberInput: "",
-//     signOperator: "",
-//     secondNumberInput: "",
-//     answer: "",
-//     // history: [],
-//   }
-// ];
+let calculations = [];
+// console.log("calculations", calculations)
 
-  let calculations = [];
-  // console.log("calculations", calculations)
+// define routes (endpoints)
+app.get('/calculations', (req, res) => {
+  console.log('in /calculations GET')
+  res.send(calculations)
+});
 
-  // define routes (endpoints)
-  app.get('/calculations', (req, res) => {
-    console.log('in /calculations GET')
-    res.send(calculations)
-  });
+//POST
+app.post('/addCalculations', (req, res) => {
+  console.log('in post', req.body);
+  let firstNumberInput = Number(req.body.firstNumberInput);
+  let sign = req.body.signOperator;
+  let secondNumberInput = Number(req.body.secondNumberInput);
+  let answerValue = 0
 
-  //POST
-  app.post('/addCalculations', (req, res) => { 
-    console.log('in post', req.body);
-    let firstNumberInput = Number(req.body.firstNumberInput);
-    let sign = req.body.signOperator;
-    let secondNumberInput = Number(req.body.secondNumberInput);
-    let answerValue = 0
-    
 
-    // calculations.push(req.body);
-    console.log("firstNumberInput", firstNumberInput)
-    console.log("secondNumberInput", secondNumberInput)
-    
-    //LOGIC GOES HERE
- 
-      if (sign === '+') {
-        console.log("sign", sign)
-        answerValue = firstNumberInput + secondNumberInput;
-        console.log("answer", answerValue)
-        req.body.answer = answerValue;
-        console.log(req.body)
-        calculations.push(req.body);
-      }
-      else if (sign === '-') {
-          console.log("sign", sign)
-          answerValue = firstNumberInput - secondNumberInput;
-          console.log("answer", answerValue)
-          req.body.answer = answerValue;
-          console.log(req.body)
-          calculations.push(req.body);
-      }
-      else if (sign === '*') {
-            console.log("sign", sign)
-            answerValue = firstNumberInput * secondNumberInput;
-            console.log("answer", answerValue)
-            req.body.answer = answerValue;
-            console.log(req.body)
-            calculations.push(req.body);
-      }
-      else if (sign === '/') {
-              console.log("sign", sign)
-              answerValue = firstNumberInput / secondNumberInput;
-              console.log("answer", answerValue)
-              req.body.answer = answerValue;
-              console.log(req.body)
-              calculations.push(req.body);
+  // calculations.push(req.body);
+  console.log("firstNumberInput", firstNumberInput)
+  console.log("secondNumberInput", secondNumberInput)
 
-     }
-    console.log("answer", req.body.answer)
-    console.log("qnswerValue", answerValue)
+  //LOGIC GOES HERE
 
-    res.sendStatus(201);
+  if (sign === '+') {
+    console.log("sign", sign)
+    answerValue = firstNumberInput + secondNumberInput;
+    console.log("answer", answerValue)
+    req.body.answer = answerValue;
+    console.log(req.body)
+    calculations.push(req.body);
+  }
+  else if (sign === '-') {
+    console.log("sign", sign)
+    answerValue = firstNumberInput - secondNumberInput;
+    console.log("answer", answerValue)
+    req.body.answer = answerValue;
+    console.log(req.body)
+    calculations.push(req.body);
+  }
+  else if (sign === '*') {
+    console.log("sign", sign)
+    answerValue = firstNumberInput * secondNumberInput;
+    console.log("answer", answerValue)
+    req.body.answer = answerValue;
+    console.log(req.body)
+    calculations.push(req.body);
+  }
+  else if (sign === '/') {
+    console.log("sign", sign)
+    answerValue = firstNumberInput / secondNumberInput;
+    console.log("answer", answerValue)
+    req.body.answer = answerValue;
+    console.log(req.body)
+    calculations.push(req.body);
 
-  });
+  }
+  console.log("answer", req.body.answer)
+  console.log("qnswerValue", answerValue)
+
+  res.sendStatus(201);
+
+});
